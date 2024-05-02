986ea31fbe4d91a361f6fb69ed3ddff97455f723
Mykhailo-Andreiev <mykhailo.andreiev@student.karazin.ua>
2024-05-02 09:55:37 +0300

    Initial commit

---
+++ src/tcpWork/AddMetroCardOperation.java
@@ -0,0 +1,17 @@
+package tcpWork;
+
+
+
+public class AddMetroCardOperation extends CardOperation {
+    private final MetroCard crd;
+
+    public AddMetroCardOperation() {
+        crd = new MetroCard();
+    }
+
+    public MetroCard getCrd() {
+        return crd;
+    }
+}
+
+
